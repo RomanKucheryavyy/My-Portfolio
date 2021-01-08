098b9b642fd885a4d92829c58de9e2aa326282bf
Roman Kucheryavyy <kucheryavyyroman@gmail.com>
2021-01-07 17:28:05 -0800

    Create projects_data.js

---
+++ dev_portfolio/src/components/data/projects_data.js
@@ -0,0 +1,28 @@
+import tweetme from '../../assets/projects_images/tweetme.PNG'
+import chatapp from '../../assets/projects_images/chatapp.JPG'
+import hardware_store from '../../assets/projects_images/hardware_store.jpg'
+import portfolio from '../../assets/projects_images/portfolio.JPG'
+import new_year from '../../assets/projects_images/new_year.jpg'
+import color_classification from '../../assets/projects_images/color_classification.jpg'
+import blinking from '../../assets/projects_images/blinking.JPG'
+import overlift from '../../assets/projects_images/overlift.jpg'
+import Dev_talks from '../../assets/projects_images/Dev_talks.jpg'
+
+const data_projects = [
+    {
+        name: 'Overlift App',
+        image: overlift,
+        deployed_url: 'https://www.youtube.com/watch?v=SYhgPC3FIJk',
+        github_url: 'https://github.com/RomanKucheryavyy/OverliftApp',
+        category: ['javascript']
+    },
+    {
+        name: 'Dev Portfolio',
+        image: portfolio,
+        deployed_url: 'https://github.com/RomanKucheryavyy/My-Portfolio',
+        category: ['vanilla']
+    },
+
+]
+
+export default data_projects;
